6a311414cc2580097239783084edd6b0dd054161
Max Muller <maxmuller@gmail.com>
2015-10-22 15:54:14 -0700

    Pass first spec test.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
 var addAy = function(word) {
-  word = word + "ay";
+  var translation = (word + "ay");
+  return translation;
+  debugger;
 }
-
 
 
 $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     var word = $("input#word").val();
     var translation = addAy(word);
-debugger;
     $(".word").text(word);
     $(".translation").text(translation);
 
-  });
-
     $("#result").show();
     event.preventDefault();
+  });
 });
